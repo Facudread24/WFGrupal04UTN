2304811cc35727f82d9bcd99e0945fc3b152e77b
Facudread24 <rivasfacundo@hotmail.com>
2021-04-22 20:33:27 -0300

    V0.2
    Con sergio realizamos la implentacion de una nueva estructura de consultas mas dinamica, Terminamos el punto 1 y tambien realizamos el punto 2, El ejercicio 1 esta terminado,

--- Ejercicio01.aspx
+++ Ejercicio01.aspx
@@ -11,14 +11,14 @@
     <form id="form1" runat="server">
         <div>
         <p>DESTINO INICIO</p>
-            <p>PROVINCIA:<asp:DropDownList ID="ddl_prov_ini" runat="server" OnSelectedIndexChanged="ddl_prov_ini_SelectedIndexChanged">
+            <p>PROVINCIA:<asp:DropDownList ID="ddl_prov_ini" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddl_prov_ini_SelectedIndexChanged">
 				</asp:DropDownList>
 			</p>
             <p>LOCALIDAD:<asp:DropDownList ID="ddl_local_ini" runat="server">
 				</asp:DropDownList>
 			</p>
             <p>DESTINO FINAL</p>
-            <p>PROVINCIA:<asp:DropDownList ID="ddl_prov_fin" runat="server">
+            <p>PROVINCIA:<asp:DropDownList ID="ddl_prov_fin" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddl_prov_fin_SelectedIndexChanged">
 				</asp:DropDownList>
 			</p>
             <p>LOCALIDAD:<asp:DropDownList ID="ddl_local_fin" runat="server">
--- Ejercicio01.aspx.cs
+++ Ejercicio01.aspx.cs
@@ -14,58 +14,52 @@ namespace WFGrupal04UTN
 		protected void Page_Load(object sender, EventArgs e)
 		{
 			if(IsPostBack==false){
-			SqlConnection Cn_V = new SqlConnection("Data Source=localhost\\MSSQLSERVER2;Initial Catalog = Viajes;Integrated Security = True");
-			Cn_V.Open();
 
-				//DESTINO INICIO
-				SqlDataAdapter adapter_V = new SqlDataAdapter("select * from Provincias",Cn_V);
-				DataSet ds_V = new DataSet();
-
-				adapter_V.Fill(ds_V, "Provincias");
-				
-				ddl_prov_ini.DataSource = ds_V.Tables["Provincias"];
+				ddl_prov_ini.DataSource = Consulta("select* from Provincias");
 				ddl_prov_ini.DataTextField = "NombreProvincia";
 				ddl_prov_ini.DataValueField = "IdProvincia";
 				ddl_prov_ini.DataBind();
-			
-				/*adapter_V_local.Fill(ds_V, "Localidades");
-				ddl_local_ini.DataSource = ds_V.Tables["Localidades"];
-				ddl_local_ini.DataTextField = "NombreLocalidad";
-				ddl_local_ini.DataValueField = "IdLocalidad";
-				ddl_local_ini.DataBind();
-				*/
-				//DESTINO FINAL
-
-				ddl_prov_fin.DataSource = ds_V.Tables["Provincias"];
-				ddl_prov_fin.DataTextField = "NombreProvincia";
-				ddl_prov_fin.DataValueField = "IdProvincia";
-				ddl_prov_fin.DataBind();
-				/*
-				ddl_local_fin.DataSource = ds_V.Tables["Localidades"];
-				ddl_local_fin.DataTextField = "NombreLocalidad";
-				ddl_local_fin.DataValueField = "IdLocalidad";
-				ddl_local_fin.DataBind();
-				*/
-				Cn_V.Close();
+				ddl_prov_ini.Items.Insert(0, new ListItem("SELECCIONAR PROVINCIA", "0"));
 
 			}
 		}
 
+		public DataSet Consulta(string querySql)
+		{
+			string srtconn = "Data Source=localhost\\MSSQLSERVER2;Initial Catalog = Viajes;Integrated Security = True";
+			SqlConnection Cn_V = new SqlConnection(srtconn);
+			Cn_V.Open();
+			SqlCommand cmd = new SqlCommand(querySql, Cn_V);
+			SqlDataAdapter adapter_V_local = new SqlDataAdapter(cmd);
+			DataSet ds_V = new DataSet();
+			adapter_V_local.Fill(ds_V);
+			Cn_V.Close();
+			return ds_V;
+		}
 		protected void ddl_prov_ini_SelectedIndexChanged(object sender, EventArgs e)
 		{
-			/*SqlConnection Cn_V = new SqlConnection("Data Source=localhost\\MSSQLSERVER2;Initial Catalog = Viajes;Integrated Security = True");
-			Cn_V.Open();
-			SqlDataAdapter adapter_V_local = new SqlDataAdapter($"select * from Localidades where Localidades.IdProvincia={int.Parse(ddl_prov_ini.SelectedValue)}", Cn_V);
-			System.Diagnostics.Debug.WriteLine(int.Parse(ddl_prov_ini.SelectedValue));
-			DataSet ds_V = new DataSet();
-
-			adapter_V_local.Fill(ds_V, "Localidades");
-			ddl_local_ini.DataSource = ds_V.Tables["Localidades"];
+			int provId = int.Parse(ddl_prov_ini.SelectedValue);
+			ddl_local_ini.DataSource = Consulta($"select * from Localidades where Localidades.IdProvincia={provId}");
 			ddl_local_ini.DataTextField = "NombreLocalidad";
 			ddl_local_ini.DataValueField = "IdLocalidad";
 			ddl_local_ini.DataBind();
-			Cn_V.Close();
-			*/
+			ddl_local_ini.Items.Insert(0,new ListItem("[SELECCIONAR LOCALIDAD]", "0"));
+
+			ddl_prov_fin.DataSource = Consulta($"select * from Provincias where IdProvincia!={provId}");
+			ddl_prov_fin.DataTextField = "NombreProvincia";
+			ddl_prov_fin.DataValueField = "IdProvincia";
+			ddl_prov_fin.DataBind();
+			ddl_prov_fin.Items.Insert(0, new ListItem("[SELECCIONAR PROVINCIA]", "0"));
+		}
+
+		protected void ddl_prov_fin_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			int provId = int.Parse(ddl_prov_fin.SelectedValue);
+			ddl_local_fin.DataSource = Consulta($"select * from Localidades where Localidades.IdProvincia={provId}");
+			ddl_local_fin.DataTextField = "NombreLocalidad";
+			ddl_local_fin.DataValueField = "IdLocalidad";
+			ddl_local_fin.DataBind();
+			ddl_local_fin.Items.Insert(0, new ListItem("[SELECCIONAR LOCALIDAD]", "0"));
 		}
 	}
 }
