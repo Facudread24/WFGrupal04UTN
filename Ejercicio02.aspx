87b5f099bb7af6fbb0a08cb59d5200bc9adac07d
Seb-98 <sebastian98@outlook.com.ar>
2021-04-23 16:10:53 -0300

    V 0.5 Con Mati, Sergio y Enzo realizamos la mitad del ejercicio 2 Agregamos los controles al formulario, y realizamos el primer filtro de informacion, solo si escribio en los 2 textbox. Tambien agregamos la funcion de quitar filtro.

---
+++ Ejercicio02.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ejercicio02.aspx.cs" Inherits="WFGrupal04UTN.Ejercicio02" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Id Producto:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="ddlProducto" runat="server" Height="19px" Width="136px">
+                <asp:ListItem Value="=">Igual a:</asp:ListItem>
+                <asp:ListItem Value="&gt;">Mayor a:</asp:ListItem>
+                <asp:ListItem Value="&lt;">Menor a</asp:ListItem>
+            </asp:DropDownList>
+&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtProducto" runat="server"></asp:TextBox>
+            <br />
+            Id Categoria:&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="ddlCategoria" runat="server" Height="16px" Width="137px">
+                <asp:ListItem Value="=">Igual a:</asp:ListItem>
+                <asp:ListItem Value="&gt;">Mayor a:</asp:ListItem>
+                <asp:ListItem Value="&lt;">Menor a:</asp:ListItem>
+            </asp:DropDownList>
+&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtCategoria" runat="server" Width="125px"></asp:TextBox>
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnFiltrar" runat="server" OnClick="btnFiltrar_Click" Text="Filtrar" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnQuitar" runat="server" OnClick="btnQuitar_Click" Text="Quitar Filtro" />
+            <br />
+            <br />
+            <asp:GridView ID="grdDatos" runat="server">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Ejercicio02.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WFGrupal04UTN
+{
+    public partial class Ejercicio02 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(IsPostBack == false)
+            {
+                CargarDatos();
+            }
+            
+        }
+
+        void CargarDatos()
+            {
+            SqlConnection cn = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True");
+            cn.Open();
+
+            SqlCommand cmd = new SqlCommand("select * from Productos", cn);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            grdDatos.DataSource = dr;
+            grdDatos.DataBind();
+
+            cn.Close();
+            }
+
+        protected void btnFiltrar_Click(object sender, EventArgs e)
+        {
+            txtProducto.Text = txtProducto.Text.Trim();
+            txtCategoria.Text = txtCategoria.Text.Trim();
+
+            if (txtProducto.Text == "" && txtCategoria.Text == "")
+            {
+                CargarDatos();
+            }
+
+            else 
+            {
+                SqlConnection cn = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True");
+                cn.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = cn;
+                    //SqlDataReader dr;
+
+                           if(txtProducto.Text != "" && txtCategoria.Text != "")
+                            {
+                                cmd.CommandText = $"select * from Productos where IdProducto {ddlProducto.SelectedValue} {txtProducto.Text} AND IdCategoría {ddlCategoria.SelectedValue} {txtCategoria.Text}";
+                                SqlDataReader dr = cmd.ExecuteReader();
+
+                                grdDatos.DataSource = dr;
+                                grdDatos.DataBind();
+                            
+                            }
+                
+
+                cn.Close();
+            }
+        }
+
+        protected void btnQuitar_Click(object sender, EventArgs e)
+        {
+            txtProducto.Text = "";
+            txtCategoria.Text = "";
+            CargarDatos();
+        }
+    }
+}
